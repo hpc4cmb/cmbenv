57d7517d08a1bba3326d6aaa24b486f05f5523ca
Theodore Kisner <tskisner.public@gmail.com>
2019-04-24 13:24:34 -0700

    Add suitesparse package

---
+++ pkgs/suitesparse.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+pkg="suitesparse"
+pkgopts=$@
+cleanup=""
+
+pfile=SuiteSparse-5.4.0.tar.gz
+src=$(eval "@TOP_DIR@/tools/fetch_check.sh" http://faculty.cse.tamu.edu/davis/SuiteSparse/${pfile} ${pfile})
+
+if [ "x${src}" = "x" ]; then
+    echo "Failed to fetch ${pkg}" >&2
+    exit 1
+fi
+cleanup="${src}"
+
+log="../log_${pkg}"
+
+echo "Building ${pkg}..." >&2
+
+rm -rf SuiteSparse
+tar xzf ${src} \
+    && cd SuiteSparse \
+    && make library JOBS=@MAKEJ@ \
+    CC="@CC@" CXX="@CXX@" CFLAGS="@CFLAGS@" AUTOCC=no \
+    GPU_CONFIG="" \
+    CFOPENMP="@OPENMP_CXXFLAGS@" LAPACK="@LAPACK@" BLAS="@BLAS@" \
+    > ${log} 2>&1 \
+    && cp -a ./lib/* "@AUX_PREFIX@/lib/" \
+    && cp -a ./include/* "@AUX_PREFIX@/include/"
+
+if [ $? -ne 0 ]; then
+    echo "Failed to build ${pkg}" >&2
+    exit 1
+fi
+
+echo "Finished building ${pkg}" >&2
+echo "${cleanup}"
+exit 0
