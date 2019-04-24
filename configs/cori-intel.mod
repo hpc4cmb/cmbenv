6cb6b68843d70ae6722bce7a7d2db8ce248d195d
Theodore Kisner <tskisner.public@gmail.com>
2019-04-24 07:16:12 -0700

    Move nersc-specific things into machine configs rather than template

--- configs/cori-intel.mod
+++ configs/cori-intel.mod
@@ -9,6 +9,14 @@ if [ module-info mode load ] {
       module swap PrgEnv-gnu PrgEnv-intel
     }
   }
+  # altd is evil and causes random job hangs
+  if [ is-loaded altd ] {
+    module unload altd
+  }
+  # darshan is useless at best and may cause overhead
+  if [ is-loaded darshan ] {
+    module unload darshan
+  }
   module load gcc
   module load git
   setenv CRAYPE_LINK_TYPE dynamic
--- configs/cori-intel.sh
+++ configs/cori-intel.sh
@@ -2,6 +2,7 @@ loadedgnu=`${MODULESHOME}/bin/modulecmd sh -t list 2>&1 | grep PrgEnv-gnu`
 loadedintel=`${MODULESHOME}/bin/modulecmd sh -t list 2>&1 | grep PrgEnv-intel`
 loadedcray=`${MODULESHOME}/bin/modulecmd sh -t list 2>&1 | grep PrgEnv-cray`
 loadeddarshan=`${MODULESHOME}/bin/modulecmd sh -t list 2>&1 | grep darshan`
+loadedaltd=`${MODULESHOME}/bin/modulecmd sh -t list 2>&1 | grep altd`
 if [ "x${loadedintel}" = x ]; then
     if [ "x${loadedcray}" != x ]; then
       module swap PrgEnv-cray PrgEnv-intel
@@ -9,6 +10,14 @@ if [ "x${loadedintel}" = x ]; then
     if [ "x${loadedgnu}" != x ]; then
       module swap PrgEnv-gnu PrgEnv-intel
     fi
+fi
+# altd is evil and causes random job hangs
+if [ "x${loadedaltd}" != x ]; then
+  module unload altd
+fi
+# darshan is useless at best and may cause overhead
+if [ "x${loadeddarshan}" != x ]; then
+  module unload darshan
 fi
 module load gcc
 module load git
