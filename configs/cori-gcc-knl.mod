264058e1f7cee27beba756ba01c06ab148cec28d
Theodore Kisner <tskisner.public@gmail.com>
2019-04-24 21:03:27 -0700

    Add cori gcc configs

---
+++ configs/cori-gcc-knl.mod
@@ -0,0 +1,23 @@
+
+if [ module-info mode load ] {
+  if [ is-loaded PrgEnv-gnu ] {
+  } else {
+    if [ is-loaded PrgEnv-cray ] {
+      module swap PrgEnv-cray PrgEnv-gnu
+    }
+    if [ is-loaded PrgEnv-intel ] {
+      module swap PrgEnv-intel PrgEnv-gnu
+    }
+  }
+  # altd is evil and causes random job hangs
+  if [ is-loaded altd ] {
+    module unload altd
+  }
+  # darshan is useless at best and may cause overhead
+  if [ is-loaded darshan ] {
+    module unload darshan
+  }
+  module swap gcc gcc/8.2.0
+  module load git
+  setenv CRAYPE_LINK_TYPE dynamic
+}
---
+++ configs/cori-gcc-knl.sh
@@ -0,0 +1,24 @@
+loadedgnu=`${MODULESHOME}/bin/modulecmd sh -t list 2>&1 | grep PrgEnv-gnu`
+loadedintel=`${MODULESHOME}/bin/modulecmd sh -t list 2>&1 | grep PrgEnv-intel`
+loadedcray=`${MODULESHOME}/bin/modulecmd sh -t list 2>&1 | grep PrgEnv-cray`
+loadeddarshan=`${MODULESHOME}/bin/modulecmd sh -t list 2>&1 | grep darshan`
+loadedaltd=`${MODULESHOME}/bin/modulecmd sh -t list 2>&1 | grep altd`
+if [ "x${loadedgnu}" = x ]; then
+    if [ "x${loadedcray}" != x ]; then
+      module swap PrgEnv-cray PrgEnv-gnu
+    fi
+    if [ "x${loadedintel}" != x ]; then
+      module swap PrgEnv-intel PrgEnv-gnu
+    fi
+fi
+# altd is evil and causes random job hangs
+if [ "x${loadedaltd}" != x ]; then
+  module unload altd
+fi
+# darshan is useless at best and may cause overhead
+if [ "x${loadeddarshan}" != x ]; then
+  module unload darshan
+fi
+module swap gcc gcc/8.2.0
+module load git
+export CRAYPE_LINK_TYPE=dynamic
