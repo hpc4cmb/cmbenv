5c5302270070eabc15ea7cb64843c4e110427901
Theodore Kisner <tskisner.public@gmail.com>
2019-09-30 14:45:14 -0700

    Bump conviqt version and download from github now that it is public.

--- pkgs/conviqt.sh
+++ pkgs/conviqt.sh
@@ -4,8 +4,9 @@ pkg="conviqt"
 pkgopts=$@
 cleanup=""
 
-pfile=libconviqt-1.1.0.tar.bz2
-src=$(eval "@TOP_DIR@/tools/fetch_check.sh" https://www.dropbox.com/s/11r3pj4wntnqax1/libconviqt-1.1.0.tar.bz2?dl=1 ${pfile})
+version=1.2.1
+pfile=libconviqt-${version}.tar.bz2
+src=$(eval "@TOP_DIR@/tools/fetch_check.sh" https://github.com/hpc4cmb/libconviqt/releases/download/v${version}/libconviqt-${version}.tar.bz2 ${pfile})
 
 if [ "x${src}" = "x" ]; then
     echo "Failed to fetch ${pkg}" >&2
@@ -17,9 +18,9 @@ log="../log_${pkg}"
 
 echo "Building ${pkg}..." >&2
 
-rm -rf libconviqt-1.1.0
+rm -rf libconviqt-${version}
 tar xjf ${src} \
-    && cd libconviqt-1.1.0 \
+    && cd libconviqt-${version} \
     && CC="@MPICC@" CXX="@MPICXX@" MPICC="@MPICC@" MPICXX="@MPICXX@" \
     CFLAGS="@CFLAGS@ -std=gnu99" CXXFLAGS="@CXXFLAGS@" \
     OPENMP_CFLAGS="@OPENMP_CFLAGS@" OPENMP_CXXFLAGS="@OPENMP_CXXFLAGS@" \
