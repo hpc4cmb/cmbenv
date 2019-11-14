ca5ae48dbc3ccc5bf842b50ef2c2964dc3ed840a
Theodore Kisner <tskisner.public@gmail.com>
2019-11-14 15:47:39 -0800

    Bump tidas version

--- pkgs/tidas.sh
+++ pkgs/tidas.sh
@@ -4,7 +4,7 @@ pkg="tidas"
 pkgopts=$@
 cleanup=""
 
-version=0.3.4
+version=0.3.5
 pfile=tidas-${version}.tar.gz
 src=$(eval "@TOP_DIR@/tools/fetch_check.sh" https://github.com/hpc4cmb/tidas/archive/${version}.tar.gz ${pfile})
 
