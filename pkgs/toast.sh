149180d2072d9ff4fbe71087b54aea3d6390b212
Theodore Kisner <tskisner.public@gmail.com>
2020-01-21 13:30:30 -0800

    Bump toast version

--- pkgs/toast.sh
+++ pkgs/toast.sh
@@ -4,7 +4,7 @@ pkg="toast"
 pkgopts=$@
 cleanup=""
 
-version=2.3.5
+version=2.3.6
 pfile=toast-${version}.tar.gz
 src=$(eval "@TOP_DIR@/tools/fetch_check.sh" https://github.com/hpc4cmb/toast/archive/${version}.tar.gz ${pfile})
 
